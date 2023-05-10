87251811d4245524ebf351b458cee1a6a105c4d2
Guilherme Schimidt <guigoschimidt@gmail.com>
2023-05-09 22:46:27 -0300

    chore: edit jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,13 @@
 export default {
-
-  clearMocks: true,
   roots: ['<rootDir>/src'],
+  collectCoverage: false,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   testEnvironment: 'node',
+
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
+
 }
